c5b262b9733841a31f3468a99fe7d40d34de0997
Anders <anders.henricsson@gmail.com>
2022-03-28 16:11:51 +0200

    first working version

---
+++ ml_till_indta.py
@@ -0,0 +1,33 @@
+import csv
+
+def main():
+    indtaFil = "Från indta.csv"
+    mlFil = "Från Megalink.csv"
+    resultatFil = "Resultat.csv"
+    with open(mlFil, newline='',encoding='ISO-8859-1') as mlcsv:
+        mlReader = csv.reader(mlcsv, delimiter=',')
+        mlDict = {}
+        for row in mlReader:
+            if len(row)>=7:
+                mlDict[row[1]]=[row[0],*row[2:7]]
+        with open(indtaFil, newline='') as csvfile:
+            indtaReader = csv.reader(csvfile, delimiter=';', quotechar='"')
+            with open(resultatFil,'w', newline='') as results:
+                writer = csv.writer(results, delimiter=';',quotechar='"')
+                for n,row in enumerate(indtaReader):
+                    name = row[9].upper()+ " " +row[8]
+                    if name in mlDict:
+                        mlRow = mlDict[name]
+                        plac = mlRow[0]
+                        series = mlRow[3].replace('.',',').split()
+                        total = mlRow[4]
+                        inner = mlRow[5]
+                        outRow = row[:29]+[plac] + series + [total, inner] + ['0','','']
+                        writer.writerow(outRow)
+                    else:
+                        writer.writerow([row[0].replace('"','')]+row[1:])
+
+if  __name__ == "__main__":
+    main()
+
+
